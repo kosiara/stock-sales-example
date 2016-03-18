def0e55088c99dac95e271bf8efeb6694f5f5700
Bartosz Kosarzycki <kosiara87@gmail.com>
2016-03-18 21:06:11 +0100

    initial project commit

---
+++ StockSales/app/src/main/java/com/bk/stocksales/MainActivity.java
@@ -0,0 +1,13 @@
+package com.bk.stocksales;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
